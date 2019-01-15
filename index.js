af94e3ebe30dae55af2ed8151719f09fed1d0e20
eGuajardoMozo <estefania_gm13@hotmail.com>
2019-01-15 10:12:31 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client({disableEveryone: true});
 
+var randomKaraTexts = ["message 1", "message 2", "message 3", "message 4", "message 5"];
+var randomTimes = [25200000, 216000000, 19800000, 18000000, 14400000 ];
 var hiResponses = ["Hello darling!", "Ah, hello my sweet angel :heart: !", "Hello my dear~", "Hello my love :heart_eyes:", "Hey there beautiful :sunglasses:"];
 var byeResponses = ["I'll be here waiting for you my love :heart:", "Take care darling, I miss you already", "See you soon my angel!", "I'll see you later beautiful :rose:"];
 var morningResponses = ["Good morning my sweet angel, did you sleep well?", "Good morning sunshine :heart: did you get the rest you needed?", "Good morning my darling, you slept well?", "Good morning love, did you have a good night's rest?", "Good morning flower, how are you feeling on this brand new morning? :sparkles:"];
@@ -16,9 +18,10 @@ bot.on('message', function(message) {
     if (message.content === "$talk") { 
         var interval = setInterval (function () {
             // use the message's channel (TextChannel) to send a new message
-            message.channel.send("123")
+			var randomKaraAux = randomKaraTexts[Math.floor(Math.random()*randomKaraTexts.length)];
+            message.channel.send(randomKaraAux)
             .catch(console.error); // add error handling here
-        }, 1000); 
+        }, 3000); 
     }
 });
 
