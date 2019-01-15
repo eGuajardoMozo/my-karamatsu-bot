db77192c69f5cc4b7813afdf996bdf8c6925b6c5
eGuajardoMozo <estefania_gm13@hotmail.com>
2019-01-14 21:03:09 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const bot = new Discord.Client({disableEveryone: true});
 
 var hiResponses = ["Hello darling!", "Ah, hello my sweet angel :heart: !", "Hello my dear~", "Hello my love :heart_eyes:"];
-var i = hiResponses.length;
+var i = Math.floor(Math.random() * hiResponses.length);
 
 bot.on('ready', function (evt) {
   console.log(`${bot.user.username} is online!`);
