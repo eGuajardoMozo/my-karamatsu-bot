785e5a95482a84b3f3546c4d11f7e499a7141b61
eGuajardoMozo <estefania_gm13@hotmail.com>
2019-01-15 09:10:56 -0600

    Bye and better greeting responses

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const Discord = require("discord.js");
 const bot = new Discord.Client({disableEveryone: true});
 
 var hiResponses = ["Hello darling!", "Ah, hello my sweet angel :heart: !", "Hello my dear~", "Hello my love :heart_eyes:", "Hey there beautiful :sunglasses:"];
-var morningResponses = ["Good morning my sweet angel", "Good morning sunshine :heart:", "Good morning sleepyhead", "Good morning love", "Good morning flower"];
-var nightResponses = ["Sleep tight darling", "Sweet dreams my angel", "Good night love :heart:", "Good night, I'll see you in my dreams darling", "Until tomorrow my sweet, good night"];
+var byeResponses = ["I'll be here waiting for you my love :heart:", "Take care darling, I miss you already", "See you soon my angel!", "I'll see you later beautiful :rose:"];
+var morningResponses = ["Good morning my sweet angel, did you sleep well?", "Good morning sunshine :heart: did you get the rest you needed?", "Good morning my darling, you slept well?", "Good morning love, did you have a good night's rest?", "Good morning flower, how are you feeling on this brand new morning? :sparkles:"];
+var nightResponses = ["Sleep tight darling, I'll see you tomorrow", "Sweet dreams my angel", "Good night love :heart:", "Good night, I'll see you in my dreams darling", "Until tomorrow my sweet, good night"];
 
 bot.on('ready', function (evt) {
   console.log(`${bot.user.username} is online!`);
@@ -11,14 +12,21 @@ bot.on('ready', function (evt) {
 });
 
 bot.on('message', async message => {
-	
+	//so he won't reply to himself
 	if (message.author.bot) return;
 	
+	//Greetings and goodbyes
     if( message.content.includes('hi') || message.content.includes('Hi') || message.content.includes('Hello') || 
 	message.content.includes('hello') || message.content.includes('Hey') || message.content.includes('hey')) {
 		var hiAux = hiResponses[Math.floor(Math.random()*hiResponses.length)];
         message.reply(hiAux);
     }
+	
+	if (message.content.includes('bye') || message.content.includes('Bye') || message.content.includes('I have to go') || 
+	message.content.includes('i have to go') || message.content.includes('i gotta go') || message.content.includes('I gotta go')) {
+		var byeAux = byeResponses[Math.floor(Math.random()*byeResponses.length)];
+		message.reply(byeAux);
+	}
 	
 	if (message.content.includes('mornin') || message.content.includes('Mornin') || message.content.includes('good morning') || 
 	message.content.includes('Good morning')) {
@@ -30,6 +38,7 @@ bot.on('message', async message => {
 		var nightAux = nightResponses[Math.floor(Math.random()*nightResponses.length)];
 		message.reply(nightAux);
 	}
+	
 });
 
 bot.login(process.env.token);
