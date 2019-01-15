4758c92286c130c7ff4c501ad798c694c50cd653
eGuajardoMozo <estefania_gm13@hotmail.com>
2019-01-15 08:54:08 -0600

    morning and night

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const Discord = require("discord.js");
 const bot = new Discord.Client({disableEveryone: true});
 
 var hiResponses = ["Hello darling!", "Ah, hello my sweet angel :heart: !", "Hello my dear~", "Hello my love :heart_eyes:", "Hey there beautiful :sunglasses:"];
+var morningResponses = ["Good morning my sweet angel", "Good morning sunshine :heart:", "Good morning sleepyhead", "Good morning love", "Good morning flower"];
+var nightResponses = ["Sleep tight darling", "Sweet dreams my angel", "Good night love :heart:", "Good night, I'll see you in my dreams darling", "Until tomorrow my sweet, good night"];
 
 bot.on('ready', function (evt) {
   console.log(`${bot.user.username} is online!`);
@@ -17,6 +19,17 @@ bot.on('message', async message => {
 		var hiAux = hiResponses[Math.floor(Math.random()*hiResponses.length)];
         message.reply(hiAux);
     }
+	
+	if (message.content.includes('mornin') || message.content.includes('Mornin') || message.content.includes('good morning') || 
+	message.content.includes('Good morning')) {
+		var morningAux = morningResponses[Math.floor(Math.random()*morningResponses.length)];
+		message.reply(morningAux);
+	}
+	
+	if (message.content.includes('good night') || message.content.includes('Good night')) {
+		var nightAux = nightResponses[Math.floor(Math.random()*nightResponses.length)];
+		message.reply(nightAux);
+	}
 });
 
 bot.login(process.env.token);
