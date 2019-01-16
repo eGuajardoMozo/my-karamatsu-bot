793507765f78e500be738739bf5ae8ce8eb7fba0
eGuajardoMozo <estefania_gm13@hotmail.com>
2019-01-16 09:32:47 -0600

    Update index.js

--- index.js
+++ index.js
@@ -180,7 +180,7 @@ bot.on('message', async message => {
 		}
 		
 		//Sending an emoji
-		else if (message.content == "❤️") {
+		else if (message.content.startsWith(":heart:")) {
 			console.log("im in");
 			var emojiAux = emojiResponses[Math.floor(Math.random()*emojiResponses.length)];
 			message.reply(emojiAux);
