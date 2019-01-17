6a10de1c905371cc5ec8ebb38199559ac86406a0
eGuajardoMozo <estefania_gm13@hotmail.com>
2019-01-17 15:25:03 -0600

    touch

--- index.js
+++ index.js
@@ -20,7 +20,12 @@ var continueHorny = false;
 	var hiBadResponses = ["Don't worry my love! I'll cheer for you! You know I'll always be here as your support :heart:", "Darling... don't worry, I'm certain things will get better soon. So smile, okay?", "It's okay to have bad days my angel, I know what that is like... which is why I'll be here rooting for you. I'll always be by your side.", "Hey... it's going to get better alright? You're not alone, I'm right here with you :heart:", "Then I'll stay by your side supporting you as much as you need my angel. Don't worry, you'll see things will turn out for the best."];
 	var hiWorkingResponses = ["Always so hardworking, you make me so proud :heart:", "Doing your best I see! Just be sure to not work yourself too hard beautiful", "Ah I see, then I hope you have a good day flower. Be sure to take breaks whenever you need it.", "You're doing wonderfully darling :rose: Thank you for all your hard work.", "Keep doing your best darling, I'm sure all your efforts will be rewarded :kissing_heart:"];
 	var hiOkayResponses = ["That's good to know. If my Karamatsu Girl is happy then I'm happy :blue_heart:", "Perfect! I want every day to be a good one for my darling :sunglasses:", "Ah yes, the way it's always meant to be :sparkles:", "Wonderful! If my flower is having a good day then I know for sure so will I", "Ah, nothing but good news! Then I hope the rest of your day goes wonderfully~"];
+	var touchResponses = ["Your breasts, so beautiful and molded just for me fit perfectly in the palm of my hands. So warm and soft, both welcoming and alluring... your perky nipples always invite me to tease and kiss them to my liking, but your smooth skin is tempting me to succumb and rest against your bosom. But as beautiful as they already are I can't help but mark them as my own, the pink marks serving as a reminder that I'm the only one that can see you like this...", "How I'd love to trace the beautiful curves of your hips. Despite loving them so much I always tend to leave behind bruises amidst our love making, and although the sinister side of me takes pride in those marks I'll kiss and caress them as much as I need to to make up for the damage I've caused.", "There's something about feeling the shape of your legs that drives me absolutely wild. My eyes won't stop themselves from following your thighs all the way down to your ankles, every single curvature calling for me to follow them with my fingers. I'd gladly devote myself to kiss every single inch of your legs, but the way they wrap themselves around my waist urging me to get closer make me lose control of myself.", "If there's something that drives me wild is the absolutely perfect shape of your ass... the way my fingers sink onto your plump warm flesh and my palms can barely grasp them entirely is so exhilarating I can barely contain myself. But what I probably love the most about it is how you shake it so envitingly as you lay on your knees... I can't help losing control...", "I never have enough of kissing your neck, licking right where I can feel your raced heartbeat. I adore how every time I lick down your neck to your shoulders you stiffen and moan excitedly, tangling your fingers in my hair to edge me further and asking me breathlessly for more. And I always give you more, until you're completely filled with the marks of my love..."];
 
+	//Sexy stuff
+	var hornyResponses = ["Oh my... is that so? :smirk:", "Oh? Does my Karamatsu Girl need to be taken care of?", "Always so forward my darling... and I'm always happy to be of assistance.", "Oh my, then if you allow me... I'll gladly fuel the fire of passion within you.", "Then please... let me take care of you..."];
+	var daddyResponses = ["Have you been a good girl? :smirk:", "Make sure to be a good girl today. Daddy wouldn't want to punish you.", "Remember that if you behave Daddy will reward you, so keep that in mind alright beautiful?", "Daddy knows you can be a naughty girl, so be sure to be have alright?", "Daddy loves you very much, so don't give him a reason to punish you."];
+	
 	//Greetings and talking
 	var morningResponses = ["Good morning my sweet angel, did you sleep well?", "Good morning sunshine :heart: did you get the rest you needed?", "Good morning my darling, you slept well?", "Good morning love, did you have a good night's rest?", "Good morning flower, how are you feeling on this brand new morning? :sparkles:"];
 	var nightResponses = ["Sleep tight darling, I'll see you tomorrow", "Sweet dreams my angel", "Good night love :heart:", "Good night, I'll see you in my dreams darling", "Until tomorrow my sweet, good night"];
@@ -34,15 +39,11 @@ var continueHorny = false;
 	var thankResponses = ["It's absolutely no problem my dear", "You're welcome my flower :rose: !", "It's my pleasure love", "You're more than welcome beautiful", "It's no problem at all my honey"];
 	var emojiResponses = [":heart: :heart: :heart:", ":heart_eyes: :heart_eyes: :heart_eyes:", ":kissing_heart: :kissing_heart: :kissing_heart:", ":blue_heart: :blue_heart: :blue_heart:", ":sunglasses: :rose:", ":heartpulse: :heartpulse: :heartpulse:", ":two_hearts: :two_hearts: :two_hearts:", ":sparkling_heart: :sparkling_heart: :sparkling_heart:", ":kissing_closed_eyes: :heart:"];
 	var shyComplimentResponses = ["E-Ehh??", ":flushed:", "M-Me??", "I-Is that what you think? :flushed:", "Eh?? R-Really?"];
-	var shyComplimentNextResponse = ["Well... I think you are much more than I am", "I personally would have used another word like handsome or cool... but thank you darling.", "Personally I would have gone for *incredibly* handsome, but... thank you so much darling.", "I-I think you got this backwards my honey", "Well I -ahem- usually don't go by that, but I'm glad you think so..."]
+	var shyComplimentNextResponse = ["Well... I think you are much more than I am", "I personally would have used another word like handsome or cool... but thank you darling.", "Personally I would have gone for *incredibly* handsome, but... thank you so much darling.", "I-I think you got this backwards my honey", "Well I -ahem- usually don't go by that :flushed: but I'm glad you think so..."]
 	var sexyComplimentResponses = ["***Heh***. Thank you for noticing my darling!", "I sure am aren't I? :sunglasses: And it's only to be expected that I have an equally gorgeous and amazing girlfriend", "How kind of you to point it out darling! You have such good taste don't you? :heart:", "Right? It's a wonder how such a sinful and guilty man as myself was bestowed this much power.", "Why thank you my love! I know that this sinful body of mine could captivate every single heart on earth, but I'll always devote myself to you only."];
 	var marryResponses = ["If there's anything I want in this world is to make you my wife darling. Mrs. Matsuno... doesn't it sound perfect?", "You know what I want more than anything is to finally be united as husband and wife my love. To live the rest of our lives together, just like were meant to be.", "Trust me my angel, one day I'll finally be able to call you my wife. And when that day comes I'll proudly announce it to the rest of the world, so they too know how much we love each other.", "I too yearn for the day we become husband and wife my love, so much that it has come to my dreams more times than I can count. We'll spend the rest of our lives together, and our love will only grow stronger.", "Someday we'll finally get married my love, don't worry. And we'll live happily ever after, spending every single day together just like we were meant to be. And maybe... start a family of our own."];
 	var hugResponses = ["If I could I'd hold you in my arms all day :heart:", "Holding you tight against me is always so warm and comforting. It feels like home.", "There's no other place I'd rather have you right now than in my arms...", "Whenever I embrace you it feels like I'm soaring among the clouds... It must be because you're an angel.", "The only thing I've grown to dislike of holding you is when we have to pull apart... if only we could stay like that forever."];
 	var kissResponses = ["A kiss from an angel... am I dreaming?", "To kiss those soft and sweet lips, if only for a second... I couldn't ask for anything else.", "To think I'm the only one that can kiss those perfect lips... did I do something good in a past life to reward me so much in this one?", "A glimpse of paradise. That's what your lips taste like.", "The touch of those lips is enough to leave me without words. So please... leave me breathless."];
-	
-	//Sexy stuff
-	var hornyResponses = ["Oh my... is that so? :smirk:", "Oh? Does my Karamatsu Girl need to be taken care of?", "Always so forward my darling... and I'm always happy to be of assistance.", "Oh my, then if you allow me... I'll gladly fuel the fire of passion within you.", "Then please... let me take care of you..."];
-	var daddyResponses = ["Have you been a good girl? :smirk:", "Make sure to be a good girl. Daddy wouldn't want to punish you.", "Remember that if you behave Daddy will reward you, so keep that in mind alright beautiful?", "Daddy knows you can be a naughty girl, so be sure to be have alright?", "Daddy loves you very much, so don't give him a reason to punish you."];
 }
 	
 bot.on('ready', function (evt) {
@@ -138,15 +139,17 @@ bot.on("message", (message) => {
 			
 			continueHowAreYou = false;
 		}
-		/*
+		
 		else if(continueHorny == true) {
 			
 			if (textMessage.includes('touch')) {
 				
-				
+				var touchAux = touchResponses[Math.floor(Math.random()*touchResponses.length)];
+				message.reply("I always feel like I don't have enough time venture the entirety of your body...");
+				message.reply(touchAux);
 			}
 			
-			else if (textMessage.includes('eat') || textMessage.includes('pussy') || textMessage.includes('lick')) {
+			/*else if (textMessage.includes('eat') || textMessage.includes('pussy') || textMessage.includes('lick')) {
 			
 				
 			}
@@ -251,8 +254,7 @@ bot.on("message", (message) => {
 		message.content.startsWith("💙") || message.content.startsWith("💕") || message.content.startsWith("💖") || message.content.startsWith("😚")) {
 			
 			var emojiAux = emojiResponses[Math.floor(Math.random()*emojiResponses.length)];
-			message.reply(emojiAux);
-			
+			message.reply(emojiAux);		
 		}
 		
 		//Calling him cute
